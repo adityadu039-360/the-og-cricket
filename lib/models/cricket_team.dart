862599f0a76796a8c9fff271f365f1a9a2809745
adityadu039-360 <adityadu039@gmail.com>
2026-08-09 09:32:36 +0530

    Add cricket team data model

---
+++ lib/models/cricket_team.dart
@@ -0,0 +1,21 @@
+class CricketTeam {
+  final String id;
+  final String name;
+  final String shortName;
+  final String city;
+  final String emoji;
+  final String primaryColor;
+  final String secondaryColor;
+  final String logoAsset;
+
+  const CricketTeam({
+    required this.id,
+    required this.name,
+    required this.shortName,
+    required this.city,
+    required this.emoji,
+    required this.primaryColor,
+    required this.secondaryColor,
+    required this.logoAsset,
+  });
+}
